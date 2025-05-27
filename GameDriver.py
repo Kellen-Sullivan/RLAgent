7e361cebe9886264f36900e5b5b9f7e1adedc201
Kellen-Sullivan <kellen.r.sullivan@gmail.com>
2025-05-26 20:31:46 -0700

    add heuristic player, and many more updates

--- GameDriver.py
+++ GameDriver.py
@@ -1,59 +1,73 @@
 from Players import *
 import sys
 from Board import *
+import matplotlib.pyplot as plt
 
 class GameDriver:
-    def __init__(self, p1type="agent", p2type="random", num_of_games=100, learning_rate=1): # maybe add more inputs here
+    def __init__(self, p1type="random", p2type="random", num_of_games=100, learning_rate=1, display=True): # maybe add more inputs here
         if p1type == "learning":
             self.p1 = LearningPlayer("X")
         elif p1type == "random":
             self.p1 = RandomPlayer("X")
         elif p1type == "human":
             self.p1 = HumanPlayer("X")
+        elif p1type == "heuristic":
+            self.p1 = HeuristicPlayer("X")
         else:
             print("Invalid player 1 type")
             exit(-1)
-
-        if p2type == "human":
+        if p2type == "learning":
+            self.p2 = LearningPlayer("O")
+        elif p2type == "human":
             self.p2 = HumanPlayer("O")
         elif p2type == "random":
             self.p2 = RandomPlayer("O")
+        elif p2type == "heuristic":
+            self.p2 = HeuristicPlayer("O")
         else:
             print("Invalid player 2 type")
             exit(-1)
 
+        if display == "True":
+            self.display = True
+        else:
+            self.display = False
+
         self.board = Board()
         self.num_of_games = int(num_of_games)
-        self.learning_rate = int(learning_rate)
+        self.learning_rate = float(learning_rate)
 
     def process_move(self, curr_player):
         invalid_move = True
         while(invalid_move):
-            (row, col) = curr_player.get_move(self.board)
+            (row, col) = curr_player.get_move(self.board, self.learning_rate)
             if (not self.board.is_valid_move(row, col)):
-                print("Invalid move")
-                return 1 # TEMP TEST
+                if self.display:
+                    print("Invalid move")
             else:
-                print("Move:", [row, col], "\n")
+                if self.display:
+                    print("Move:", [row, col], "\n")
                 self.board.play_move(row,col,curr_player.symbol)
-                return 0 # TEMP TEST
+                return
 
     def run(self):
         self.board.clear_board()
         toggle = 0
         curr_player = self.p1 # player 1 starts the game
-        self.board.display()
-        while(True):
-            if self.process_move(curr_player) == 1: # TEMP TEST
-                print("ERROR") # TEMP TEST
-                return
+        if self.display:
             self.board.display()
+        while(True):
+            self.process_move(curr_player)
+            if self.display: 
+                self.board.display()
             # check winner or tie
             if self.board.check_win(curr_player.get_symbol()):
-                print("Player ", curr_player.get_symbol(), " wins!")
+                if self.display: 
+                    print("Player ", curr_player.get_symbol(), " wins!")
                 return 1 if curr_player.get_symbol() == "X" else -1
             if self.board.check_tie():
-                print("Game ends in a tie!")
+                if self.display:
+                    print("Game ends in a tie!")
                 return 0
             # toggle the current player
             toggle += 1
@@ -62,17 +76,45 @@ class GameDriver:
             else:
                 curr_player = self.p2
 
+def make_plots(games_played, win_percentages, boards_searched, learn):
+    # win percentage
+    plt.plot(games_played, win_percentages)
+    plt.xlabel("Games Played")
+    plt.ylabel("Win Percentage")
+    plt.title("Player 1 Win Percentage Over Time")
+    plt.show()
+    
+    if learn:
+        # boards searched
+        plt.plot(games_played, boards_searched)
+        plt.xlabel("Games Played")
+        plt.ylabel("Boards Searched")
+        plt.title("Player 1 Boards Searched Over Time")
+        plt.show()
+
+
 def main():
-    game = GameDriver(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    game = GameDriver(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    learn = sys.argv[1] == "learning" 
     player_X_wins, player_O_wins = 0, 0
+    games_played, win_percentages, boards_searched = [], [], []
     for i in range(game.num_of_games):
         winner = game.run()
         if winner == 1:
             player_X_wins += 1
         elif winner == -1:
             player_O_wins += 1
+        if i % (game.num_of_games // 10) == 0 and i != 0:
+            game.learning_rate = game.learning_rate * 0.85
+            games_played.append(i)
+            win_percentages.append(((player_X_wins / i) * 100))
+            if learn:
+                boards_searched.append(len(game.p1.values.keys()))
     print(f"player X won {player_X_wins}/{game.num_of_games} games ({((player_X_wins / game.num_of_games) * 100):.2f}%)")
     print(f"player O won {player_O_wins}/{game.num_of_games} games ({((player_O_wins / game.num_of_games) * 100):.2f}%)")
     print(f"There were {game.num_of_games - player_X_wins - player_O_wins} ties!")  
+    if learn:
+        print(f"boards searched: {len(game.p1.values.keys())}")
+    make_plots(games_played, win_percentages, boards_searched, learn)
   
 main()
--- Players.py
+++ Players.py
@@ -7,11 +7,63 @@ class Player:
     def get_symbol(self):
         return self.symbol
     
+    def get_board_string_value(self, board_str):
+        if self.get_symbol() == "X":
+            # check if X won
+            if board_str[0:3] == "XXX" or board_str[3:6] == "XXX" or board_str[6:] == "XXX":
+                return 1
+            for i in range(3):
+                if board_str[0+i] == "X" and board_str[3+i] == "X" and board_str[6+i] == "X":
+                    return 1
+            if board_str[0] == "X" and board_str[4] == "X" and board_str[8] == "X":
+                return 1
+            if board_str[2] == "X" and board_str[4] == "X" and board_str[6] == "X":
+                return 1
+            # check if O won
+            if board_str[0:3] == "OOO" or board_str[3:6] == "OOO" or board_str[6:] == "OOO":
+                return 0
+            for i in range(3):
+                if board_str[0+i] == "O" and board_str[3+i] == "O" and board_str[6+i] == "O":
+                    return 0
+            if board_str[0] == "O" and board_str[4] == "O" and board_str[8] == "O":
+                return 0
+            if board_str[2] == "O" and board_str[4] == "O" and board_str[6] == "O":
+                return 0
+        if self.get_symbol() == "O":
+            # check if X won
+            if board_str[0:3] == "XXX" or board_str[3:6] == "XXX" or board_str[6:] == "XXX":
+                return 0
+            for i in range(3):
+                if board_str[0+i] == "X" and board_str[3+i] == "X" and board_str[6+i] == "X":
+                    return 0
+            if board_str[0] == "X" and board_str[4] == "X" and board_str[8] == "X":
+                return 0
+            if board_str[2] == "X" and board_str[4] == "X" and board_str[6] == "X":
+                return 0
+            # check if O won
+            if board_str[0:3] == "OOO" or board_str[3:6] == "OOO" or board_str[6:] == "OOO":
+                return 1
+            for i in range(3):
+                if board_str[0+i] == "O" and board_str[3+i] == "O" and board_str[6+i] == "O":
+                    return 1
+            if board_str[0] == "O" and board_str[4] == "O" and board_str[8] == "O":
+                return 1
+            if board_str[2] == "O" and board_str[4] == "O" and board_str[6] == "O":
+                return 1
+        # check if tie
+        if " " not in board_str:
+            return 0
+        # no winner or tie
+        return 0.5
+    
+    def get_move_string_index(self, row, col):
+        return (3 * row) + col
+    
 class HumanPlayer(Player):
     def __init__(self, symbol):
         Player.__init__(self, symbol)
     
-    def get_move(self, board):
+    def get_move(self, board, lr):
         row = int(input("Enter row: "))
         col = int(input("Enter col: "))
         return (row, col)
@@ -20,18 +72,68 @@ class RandomPlayer(Player):
     def __init__(self, symbol):
         Player.__init__(self, symbol)
 
-    def get_move(self, board):
+    def get_move(self, board, lr):
         possible_moves = board.get_valid_moves()
         n = random.randint(0,len(possible_moves)-1)
         return possible_moves[n]
+    
+class HeuristicPlayer(Player):
+    def __init__(self, symbol):
+        Player.__init__(self, symbol)
+
+    def check_opp_win(self, board_str):
+        if self.get_symbol() == "O":
+            # check if opponent could win in next move
+            if (board_str[0:3].count("X") == 2 and board_str[0:3].count(" ") == 1) or (board_str[3:5].count("X") == 2 and board_str[3:5].count(" ") == 1) or (board_str[6:].count("X") == 2 and board_str[6:].count(" ") == 1):
+                return True
+            for i in range(3):
+                if (board_str[0+i] == "X" and board_str[3+i] == "X" and board_str[6+i] == " ") or (board_str[0+i] == "X" and board_str[3+i] == " " and board_str[6+i] == "X") or (board_str[0+i] == " " and board_str[3+i] == "X" and board_str[6+i] == "X"):
+                    return True
+            if (board_str[0] == "X" and board_str[4] == "X" and board_str[8] == " ") or (board_str[0] == "X" and board_str[4] == " " and board_str[8] == "X") or (board_str[0] == " " and board_str[4] == "X" and board_str[8] == "X"):
+                return True
+            if (board_str[2] == "X" and board_str[4] == "X" and board_str[6] == " ") or (board_str[2] == "X" and board_str[4] == " " and board_str[6] == "X") or (board_str[2] == " " and board_str[4] == "X" and board_str[6] == "X"):
+                return True
+        else:
+            # check if opponent could win in next move
+            if (board_str[0:3].count("O") == 2 and board_str[0:3].count(" ") == 1) or (board_str[3:5].count("O") == 2 and board_str[3:5].count(" ") == 1) or (board_str[6:].count("O") == 2 and board_str[6:].count(" ") == 1):
+                return True
+            for i in range(3):
+                if (board_str[0+i] == "O" and board_str[3+i] == "O" and board_str[6+i] == " ") or (board_str[0+i] == "O" and board_str[3+i] == " " and board_str[6+i] == "O") or (board_str[0+i] == " " and board_str[3+i] == "O" and board_str[6+i] == "O"):
+                    return True
+            if (board_str[0] == "O" and board_str[4] == "O" and board_str[8] == " ") or (board_str[0] == "O" and board_str[4] == " " and board_str[8] == "O") or (board_str[0] == " " and board_str[4] == "O" and board_str[8] == "O"):
+                return True
+            if (board_str[2] == "O" and board_str[4] == "O" and board_str[6] == " ") or (board_str[2] == "O" and board_str[4] == " " and board_str[6] == "O") or (board_str[2] == " " and board_str[4] == "O" and board_str[6] == "O"):
+                return True
+        return False
+
+    def get_move(self, board, lr):
+        possible_moves = board.get_valid_moves()
+        non_losing_moves = []
+        curr_board_string = board.convert_to_string()
+        for r,c in possible_moves:
+            move_idx = self.get_move_string_index(r,c)
+            new_board_string = curr_board_string[:move_idx] + self.symbol + curr_board_string[move_idx+1:]
+            curr_move_val = self.get_board_string_value(new_board_string)
+            # if move results in win, play the move
+            if curr_move_val == 0:
+                return (r,c)
+            # if opponenet wins, don't play that move
+            if self.check_opp_win(new_board_string) == False:
+                non_losing_moves.append((r,c))
+        # play random non loosing move (prioritize middle if available)
+        if len(non_losing_moves) > 0:
+            if (1,1) in non_losing_moves:
+                return (1,1)
+            else:
+                return random.choice(non_losing_moves)
+        return random.choice(possible_moves)
 
 class LearningPlayer(Player):
     """
-    TODO: Implement reinforcement learning 
-    Assume this agent always plays X's
-    Need to create a table of win probabilites for the agent
-    All states with 3 X's in a row have a win prob of 1,
-    All states with 3 O's in a row have a win prob of 0,
+    Implements reinforcement learning 
+    Creates a table of win probabilites for the agent
+    All winning states have a win prob of 1,
+    All states losing states have a win prob of 0,
     Initially set all other states to 0.5
     """
     def __init__(self, symbol):
@@ -42,52 +144,35 @@ class LearningPlayer(Player):
         """
         self.values = {"         ": 0.5}
 
-    def get_move(self, board):
+    def get_move(self, board, lr):
         """
         check all possible moves, get their values, or append new values to values dictionary
         """
         possible_moves = board.get_valid_moves()
-        # iterate through all moves and get the move with the highest value
+        # select non greedy move 10% of the time
+        if random.randint(0,100) == 0: 
+            return random.choice(possible_moves)
         best_move_val = 0
-        best_move = (0,0)
+        best_moves = []
+        # add current board to values table
         curr_board_string = board.convert_to_string()
+        if curr_board_string not in self.values.keys():
+            self.values[curr_board_string] = self.get_board_string_value(curr_board_string)
+        # iterate through all moves and get the move with the highest value
         for r,c in possible_moves:
             move_idx = self.get_move_string_index(r,c)
-            new_board_string = curr_board_string[:move_idx] + "X" + curr_board_string[move_idx+1:]
-            # if board not in values dictionary yet
+            new_board_string = curr_board_string[:move_idx] + self.get_symbol() + curr_board_string[move_idx+1:]
+            # if board resulting from curr possible move not in values dictionary yet
             if new_board_string not in self.values.keys():
                 new_board_string_val = self.get_board_string_value(new_board_string)
                 self.values[new_board_string] = new_board_string_val
-            if self.values[new_board_string] >= best_move_val:
-                best_move = (r,c)
-        return best_move
-    
-    def get_board_string_value(self, board_str):
-        # check if X won
-        if board_str[0:3] == "XXX" or board_str[3:6] == "XXX" or board_str[6:] == "XXX":
-            return 1
-        for i in range(3):
-            if board_str[0+i] == "X" and board_str[3+i] == "X" and board_str[6+i] == "X":
-                return 1
-        if board_str[0] == "X" and board_str[4] == "X" and board_str[8] == "X":
-            return 1
-        if board_str[2] == "X" and board_str[4] == "X" and board_str[6] == "X":
-            return 1
-        # check if O won
-        if board_str[0:3] == "OOO" or board_str[3:6] == "OOO" or board_str[6:] == "OOO":
-            return 0
-        for i in range(3):
-            if board_str[0+i] == "O" and board_str[3+i] == "O" and board_str[6+i] == "O":
-                return 0
-        if board_str[0] == "O" and board_str[4] == "O" and board_str[8] == "O":
-            return 0
-        if board_str[2] == "O" and board_str[4] == "O" and board_str[6] == "O":
-            return 0
-        # check if tie
-        if " " not in board_str:
-            return 0
-        # no winner or tie
-        return 0.5
-    
-    def get_move_string_index(self, row, col):
-        return (3 * row) + col
+            if self.values[new_board_string] > best_move_val:
+                best_moves = [(r,c,new_board_string)]
+                best_move_val = self.values[new_board_string]
+            elif self.values[new_board_string] == best_move_val:
+                best_moves.append((r,c,new_board_string))
+        # randomly select move from the best_moves array
+        best_move = random.choice(best_moves)
+        # update value of old state using V(s) := V(s) + a[V(s')-V(s)]
+        self.values[curr_board_string] = self.values[curr_board_string] + lr * (self.values[best_move[2]] - self.values[curr_board_string])
+        return (best_move[0], best_move[1])
